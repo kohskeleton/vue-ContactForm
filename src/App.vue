6a3315fa5053649cfc211bfddd3289fe25db6b07
kosuke kawachi <kkawachi@kk-mbp.local>
2019-07-10 09:01:18 +0900

    [mod]:add text field

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
+    <v-toolbar color="indigo" app dark>
+      <v-toolbar-title class="headline">
         <span>Sample Vue Form</span>
       </v-toolbar-title>
     </v-toolbar>
@@ -9,7 +9,11 @@
     <v-content>
       <router-view/>
     </v-content>
+
+    <v-footer color="indigo" app inset>
+      <v-layout wrap justify-center>
+        <span class="white--text">&copy; 2019</span>
+      </v-layout>
+    </v-footer>
   </v-app>
 </template>
-
-
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,41 +1,99 @@
 <template>
-  <v-container text-xs-center>
+  <v-container text-xs-center pt-5 pb-5>
 
-  <v-form ref="form" v-model="valid">
-    <v-text-field
-      v-model="name"
-      :rules="nameRules"
-      :counter="10"
-      label="名前"
-      required
-    ></v-text-field>
-    <v-text-field
-      v-model="email"
-      :rules="emailRules"
-      label="メールアドレス"
-      required
-    ></v-text-field>
-    <v-select
-      v-model="select"
-      :items="items"
-      label="どの製品について"
-    ></v-select>
-    <v-checkbox
-      v-model="checkbox"
-      :rules="[v => !!v || '続行するには同意する必要があります。']"
-      label="個人情報の保持の同意"
-      required
-    ></v-checkbox>
+    <v-form ref="form" v-model="valid">
+      <v-text-field
+        v-model="name"
+        :rules="nameRules"
+        :counter="10"
+        label="名前"
+        required
+      >
+        <template v-slot:label>
+          名前 <small>(必須)</small>
+        </template>
+      </v-text-field>
+      <v-text-field
+        v-model="kana"
+        :rules="kanaRules"
+        label="ふりがな"
+        required
+      >
+        <template v-slot:label>
+          ふりがな <small>(必須)</small>
+        </template>
+      </v-text-field>
+      <v-text-field
+        v-model="company"
+        :rules="companyRules"
+        label="社名"
+      ></v-text-field>
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        required
+      >
+        <template v-slot:label>
+          メールアドレス <small>(必須)</small>
+        </template>
+      </v-text-field>
+      <v-text-field
+        v-model="zipCode"
+        :rules="zipCodeRules"
+        label="郵便番号"
+      ></v-text-field>
+      <v-text-field
+        v-model="address"
+        :rules="addressRules"
+        label="住所"
+      ></v-text-field>
+      <v-text-field
+        v-model="tel"
+        :rules="telRules"
+        label="電話番号"
+      ></v-text-field>
+      <v-select
+        v-model="select"
+        :items="items"
+        label="どの製品について"
+      ></v-select>
+      <v-text-field
+        v-model="subject"
+        :rules="subjectRules"
+        label="件名"
+        required
+      >
+        <template v-slot:label>
+          件名 <small>(必須)</small>
+        </template>
+      </v-text-field>
+      <v-textarea
+        v-model="content"
+        :rules="contentRules"
+        label="お問い合わせ内容"
+        required
+        >
+          <template v-slot:label>
+            お問合せ内容 <small>(必須)</small>
+          </template>
+        </v-textarea>
 
-    <v-btn
-      :disabled="!valid"
-      @click="submit"
-      to="/confirm"
-    >
-      確認
-    </v-btn>
+      <v-checkbox
+        v-model="checkbox"
+        :rules="[v => !!v || '続行するには同意する必要があります。']"
+        label="個人情報の保持の同意（必須）"
+        required
+      ></v-checkbox>
 
-  </v-form>
+      <v-btn
+        :disabled="!valid"
+        @click="submit"
+        to="/confirm"
+      >
+        確認
+      </v-btn>
+
+    </v-form>
 
   </v-container>
 </template>
@@ -49,6 +107,9 @@
         v => !!v || '名前を入力してください',
         v => v.length <= 10 || '名前は10文字未満にする必要があります'
       ],
+      kanaRules: [
+        v => !!v || 'ふりがなを入力してください'
+      ],
       emailRules: [
         v => !!v || 'メールアドレスを入力してください',
         v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'メールアドレスが有効ではありません'
@@ -59,8 +120,14 @@
         'Bサービスについて',
         'Cサービスについて',
         'その他'
-      ]
+      ],
       // checkbox: false
+      subjectRules: [
+        v => !!v || '件名を入力してください'
+      ],
+      contentRules: [
+        v => !!v || 'お問い合わせ内容を入力してください'
+      ]
     }),
     computed: {
       name: {
